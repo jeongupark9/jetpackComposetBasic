fe2ad2eaaf769c89643a0f927c92485c5a608ecf
jeongupark9 <jeongupark@gmail.com>
2022-06-09 00:42:27 +0900

    text style적용

--- app/src/main/java/com/example/jetpackcomposetbasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetbasic/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -76,7 +77,9 @@ private fun Greeting(name: String) {
                 .weight(1f)
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(text = name, style = MaterialTheme.typography.h4.copy(
+                    fontWeight = FontWeight.ExtraBold
+                ))
             }
             OutlinedButton(
                 onClick = { expanded.value = !expanded.value },
