0c30c9c27d623d6bebe969334631956563cac5d0
jeongupark9 <jeongupark@gmail.com>
2022-06-09 00:34:07 +0900

    spring animation 적용

--- app/src/main/java/com/example/jetpackcomposetbasic/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetbasic/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.jetpackcomposetbasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,6 +15,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposetbasic.ui.theme.JetpackComposetBasicTheme
 
@@ -25,12 +29,13 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                   MyApp()
+                    MyApp()
                 }
             }
         }
     }
 }
+
 @Composable
 fun MyApp() {
 
@@ -42,8 +47,9 @@ fun MyApp() {
         Greetings()
     }
 }
+
 @Composable
-private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
             Greeting(name = name)
@@ -54,13 +60,21 @@ private fun Greetings(names: List<String> = List(1000) { "$it" } ) {
 @Composable
 private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f) .padding(bottom = extraPadding)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp))) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
@@ -72,8 +86,9 @@ private fun Greeting(name: String) {
         }
     }
 }
+
 @Composable
-fun OnboardingScreen(onContinueClicked: () -> Unit){
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
     Surface {
         Column(
@@ -91,6 +106,7 @@ fun OnboardingScreen(onContinueClicked: () -> Unit){
         }
     }
 }
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
@@ -98,10 +114,11 @@ fun DefaultPreview() {
         MyApp()
     }
 }
+
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun OnboardingPreview() {
     JetpackComposetBasicTheme {
-        OnboardingScreen(onContinueClicked = {  })
+        OnboardingScreen(onContinueClicked = { })
     }
 }
